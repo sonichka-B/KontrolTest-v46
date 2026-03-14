0ce9774dd05633ae0660a9e51da7121ef3ee042f
Bordachenko Sofiia <s.bordachenko@gmail.com>
2026-03-14 10:52:18 +0200

    feat:add exceptions
    Signed-off-by: Bordachenko Sofiia <s.bordachenko@gmail.com>

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,2 +1,34 @@
+import java.util.Objects;
+
 public class Email {
+    private final String email;
+
+    public Email(String email){
+        if (email == null || !email.contains("@")) {
+            throw new InvalidData("Invalid email");
+        }
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Email email1)) return false;
+        return Objects.equals(email, email1.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
 }
